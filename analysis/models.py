9f2ee5073e566b363dd87307864cfc7a9bd4354f
vector <igor.raz@list.ru>
2024-05-31 11:13:33 +0300

    create models

--- analysis/models.py
+++ analysis/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    vendor_code = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    price = models.FloatField()
+    promotion = models.FloatField()
+    date = models.DateField()
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=20)
+    url = models.URLField()
+
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+
+
+class ProductData(models.Model):
+    price = models.FloatField()
+    promotion = models.FloatField()
+    date = models.DateField()
+
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
