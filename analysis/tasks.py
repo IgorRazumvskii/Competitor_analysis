11d3bd6ecf9c82b52588f530d9a71d0dead07bac
vector <igor.raz@list.ru>
2024-06-13 14:16:54 +0300

    fix tasks.py

--- analysis/tasks.py
+++ analysis/tasks.py
@@ -10,18 +10,7 @@ from .serializers import ProductSerializerCreate
 @shared_task
 def parsing(vendor_code=0, user_id=0):
     parser = Parser()
-    # Product.objects.create(
-    #     vendor_code=vendor_code,
-    #     name=parser['name'],
-    #     price=parser['price'],
-    #     # ToDo update promotion
-    #     promotion=1,
-    #     # ToDo update store
-    #     store=Store.objects.get(name=parser['store']),
-    #     user=User.objects.get(user_id=user_id)
-    #
-    # )
-    # TODO! Добавить сюда JSON
     x = parser.run(" ", "70085281")
+    # print('\n\n\n', x, '\n\n\n')
     # s = ProductSerializerCreate(x)
     return parser.run(" ", "70085281")
